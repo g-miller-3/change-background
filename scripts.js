61af576432e387ae8278147985c300e6184b8587
g-miller-3 <grantmiller806@gmail.com>
2021-10-13 20:36:27 -0400

    Added a hexadecimal conversion for the RGB values

--- scripts.js
+++ scripts.js
@@ -5,6 +5,7 @@ document.getElementById("btn").onclick = function () {
     let b = randomIntFromInterval(0, 255);
     console.log(r,g,b);
     changeRgbText(r,g,b);
+    rgbToHex(r,g,b);
     document.body.style.background = "rgb("+r+","+g+","+b+")";
 }
 
@@ -14,4 +15,14 @@ function randomIntFromInterval(min, max) { // min and max included
 
 function changeRgbText(r, g, b) {
     document.getElementById("p1").innerHTML = "R: "+r+" "+"G: "+g+" "+"B: "+b;
+}
+
+function rgbToHex(r, g, b) {
+    let num = [r,g,b];
+    let hex = num.map(function(x) {
+        x = parseInt(x).toString(16);
+        return (x.length==1) ? "0"+x : x;
+    })
+    hex = hex.join("").toUpperCase();
+    document.getElementById("p2").innerHTML = "#"+ hex;
 }
