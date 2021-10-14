2b027108ff768fc1c546365b4ad3588ee34fb534
g-miller-3 <grantmiller806@gmail.com>
2021-10-13 20:04:52 -0400

    Added a <p> to display the current RGB value that is being displayed in the background. changeRgbText function was added to scripts to be called during the function that happens on the button click.

--- scripts.js
+++ scripts.js
@@ -4,9 +4,14 @@ document.getElementById("btn").onclick = function () {
     let g = randomIntFromInterval(0, 255);
     let b = randomIntFromInterval(0, 255);
     console.log(r,g,b);
+    changeRgbText(r,g,b);
     document.body.style.background = "rgb("+r+","+g+","+b+")";
 }
 
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function changeRgbText(r, g, b) {
+    document.getElementById("p1").innerHTML = "R: "+r+" "+"G: "+g+" "+"B: "+b;
 }
